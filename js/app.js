a919dae514e76a3e178d0060fe4a298dc76a6f7e
ADITYA K. SWAMI <84822278+SWAMIADITYA@users.noreply.github.com>
2023-01-07 09:42:35 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidestoscroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+        
+
+    });
+});
